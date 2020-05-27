56d8921999a87b3f0621a94243deab5472e60342
Liutong Chen <liutong.clt@gmail.com>
2020-05-26 23:04:36 -0700

    Duck class

---
+++ Duck/Duck.java
@@ -0,0 +1,21 @@
+public abstract class Duck {
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public Duck() {
+    }
+
+    public abstract void display();
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+}
